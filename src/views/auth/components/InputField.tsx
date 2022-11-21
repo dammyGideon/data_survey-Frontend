01697bdc67f58be75ddb002d15e2f1c5616ec82d
bakemonostan <ehizomokhomion@gmail.com>
2022-11-21 02:49:24 -0800

    login page, needs minor adjustment

--- src/views/auth/components/InputField.tsx
+++ src/views/auth/components/InputField.tsx
@@ -1,23 +1,26 @@
-import { Form } from "react-bootstrap"
-import FormVariables from "../type/Form"
+import { Form } from 'react-bootstrap';
+import FormVariables from '../type/Form';
 
+const InputField = ({
+  name,
+  label,
+  type,
+  placeholder,
+  error,
+  fieldRef,
+}: FormVariables) => {
+  return (
+    <Form.Group controlId={name} className='InputField'>
+      {label && <Form.Label>{label}</Form.Label>}
+      <Form.Control
+        type={type || 'text'}
+        placeholder={placeholder}
+        ref={fieldRef}
+      ></Form.Control>
+      {/* shouldnt this be conditionally rendered? */}
+      <Form.Text className='text-danger'>{error}</Form.Text>
+    </Form.Group>
+  );
+};
 
-const InputField=({name,label, type, placeholder,error,fieldRef} : FormVariables)=>{
-
-        return(
-                <Form.Group controlId={name} className="InputField">
-                {label && <Form.Label>{label}</Form.Label> }
-                <Form.Control 
-                type={type || 'text'} 
-                placeholder={placeholder}
-                ref={fieldRef}
-                >
-                
-                </Form.Control>
-                <Form.Text className="text-danger">{error}</Form.Text>
-            </Form.Group>
-        )
-
-}
-
-export default InputField
+export default InputField;
