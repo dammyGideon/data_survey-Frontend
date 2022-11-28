4ebd6425cecdb4a358a3a621d4a92d9cef5d4c29
bakemonostan <ehizomokhomion@gmail.com>
2022-11-28 10:54:20 +0100

    fix: fix authSide and form buttons

--- src/views/auth/components/authSider.tsx
+++ src/views/auth/components/authSider.tsx
@@ -3,17 +3,14 @@ import wave from '../../../assets/wave.svg';
 import loginImg from '../../../assets/login-img.png';
 
 const AuthSiderBar = () => {
-  return(
-  <>
-    <Stack className='mx-auto'>
-      <Col xs={3} className='mx-auto text-center'>
-        <img src={wave} alt='waving hand icon' />
-      </Col>
-      <Col className='mx-auto'>
-          <img src={wave} alt='waving hand icon' className='' />
-      </Col>
+  return (
+    <>
+      <Stack className='mx-auto'>
+        <Col xs={1} className='mx-auto text-center'>
+          <img src={wave} alt='waving hand icon' className='wave' />
+        </Col>
 
-      <Col className='text-center'>
+        <Col className='text-center'>
           <h2>Welcome back</h2>
           <p>Enter your email and password to experience wonders</p>
         </Col>
@@ -22,9 +19,8 @@ const AuthSiderBar = () => {
         <Col>
           <img src={loginImg} alt='illustration' />
         </Col>
-    </Stack>
-    
-  </>
-  )
-}
+      </Stack>
+    </>
+  );
+};
 export default AuthSiderBar;
--- src/views/auth/views/Login.tsx
+++ src/views/auth/views/Login.tsx
@@ -1,19 +1,15 @@
-import Dashboard from "../../Dashboard"
+import Dashboard from '../../Dashboard';
 import { lazy } from 'react';
 import { Button, Col, Container, Form, Row, Stack } from 'react-bootstrap';
 import { useRef } from 'react';
 import facebook from '../../../assets/facebook.svg';
 import google from '../../../assets/google.svg';
 
-
-
-
-const InputField =   lazy(() => import('../components/InputField'));
-const AuthSiderBar = lazy(()=>  import('../components/authSider'));
+const InputField = lazy(() => import('../components/InputField'));
+const AuthSiderBar = lazy(() => import('../components/authSider'));
 
 const LoginPage = () => {
-   const fieldText = useRef<HTMLInputElement | null>(null);
-
+  const fieldText = useRef<HTMLInputElement | null>(null);
 
   return (
     <main>
@@ -63,10 +59,20 @@ const LoginPage = () => {
                 </strong>
                 <Stack direction='horizontal' gap={4}>
                   <Button variant='outline-primary'>
-                    <img src={google} alt='google icon' /> Google sign in
+                    <img
+                      src={google}
+                      alt='google icon'
+                      className='google-img'
+                    />{' '}
+                    Google sign in
                   </Button>
                   <Button variant='outline-primary' className=' ms-auto'>
-                    <img src={facebook} alt='facebook icon' /> Facebook sign in
+                    <img
+                      src={facebook}
+                      alt='facebook icon'
+                      className='fb-img'
+                    />{' '}
+                    Facebook sign in
                   </Button>
                 </Stack>
               </Stack>
@@ -77,7 +83,7 @@ const LoginPage = () => {
               </div>
             </Col>
           </Col>
-          <Col>
+          <Col sm={12} md={6}>
             <AuthSiderBar />
           </Col>
         </Row>
